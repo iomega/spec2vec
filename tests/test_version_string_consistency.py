86c67e070191f7aaa60447d345e0e03a13b5ec03
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-05-14 18:23:30 +0200

    updated version string consistency test

--- tests/test_version_string_consistency.py
+++ tests/test_version_string_consistency.py
@@ -1,13 +1,13 @@
 import os
 import re
-from matchms import __version__ as expected_version
+from spec2vec import __version__ as expected_version
 
 
 def test_version_string_consistency():
-    """Check whether version in meta.yaml is consistent with that in matchms.__version__"""
+    """Check whether version in conda/meta.yaml is consistent with that in spec2vec.__version__"""
 
-    repository_root = os.path.join(os.path.dirname(__file__), '..')
-    fixture = os.path.join(repository_root, "meta.yaml")
+    repository_root = os.path.join(os.path.dirname(__file__), "..")
+    fixture = os.path.join(repository_root, "conda", "meta.yaml")
 
     with open(fixture, "r") as f:
         metayaml_contents = f.read()
@@ -15,5 +15,5 @@ def test_version_string_consistency():
     match = re.search(r"^{% set version = \"(?P<semver>.*)\" %}$", metayaml_contents, re.MULTILINE)
     actual_version = match["semver"]
 
-    assert expected_version == actual_version, "Expected version string used in meta.yaml to be consistent with" \
-                                               " that in matchms.__version__"
+    assert expected_version == actual_version, "Expected version string used in conda/meta.yaml to be consistent with" \
+                                               " that in spec2vec.__version__"

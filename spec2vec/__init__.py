7b2db6165a3f34b04a6faf3d6f275afb05625bec
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-05-14 18:37:11 +0200

    fixed order of imports

--- spec2vec/__init__.py
+++ spec2vec/__init__.py
@@ -1,9 +1,9 @@
+from .__version__ import __version__
 from .calc_vector import calc_vector
 from .Document import Document
 from .Spec2Vec import Spec2Vec
 from .Spec2VecParallel import Spec2VecParallel
 from .SpectrumDocument import SpectrumDocument
-from .__version__ import __version__
 
 
 __all__ = [

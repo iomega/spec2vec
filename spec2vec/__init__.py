dcc99945bdc652108b815de968cb6d932b264edf
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-05-14 18:22:14 +0200

    didnt expose __version__

--- spec2vec/__init__.py
+++ spec2vec/__init__.py
@@ -3,9 +3,11 @@ from .Document import Document
 from .Spec2Vec import Spec2Vec
 from .Spec2VecParallel import Spec2VecParallel
 from .SpectrumDocument import SpectrumDocument
+from .__version__ import __version__
 
 
 __all__ = [
+    "__version__",
     "calc_vector",
     "Document",
     "SpectrumDocument",

afa4ddd2f8fb2870e7dc6f0b831411e337b65787
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-06-12 16:26:34 +0200

    test linting

--- tests/test_calc_vector.py
+++ tests/test_calc_vector.py
@@ -16,8 +16,8 @@ def test_calc_vector():
     document = SpectrumDocument(spectrum, n_decimals=1)
     model = import_pretrained_model()
     vector = calc_vector(model, document, intensity_weighting_power=0.5, allowed_missing_percentage=1.0)
-    expected_vector = numpy.array([ 0.08982063, -1.43037023, -0.17572929, -0.45750666, 0.44942236,
-                                   1.35530729, -1.8305029 , -0.36850534, -0.28393048, -0.34192028])
+    expected_vector = numpy.array([0.08982063, -1.43037023, -0.17572929, -0.45750666, 0.44942236,
+                                   1.35530729, -1.8305029, -0.36850534, -0.28393048, -0.34192028])
     assert numpy.all(vector == pytest.approx(expected_vector, 1e-5)), "Expected different document vector."
 
 
@@ -46,13 +46,14 @@ def test_calc_vector_within_allowed_missing_percentage():
     document = SpectrumDocument(spectrum, n_decimals=1)
     model = import_pretrained_model()
     vector = calc_vector(model, document, intensity_weighting_power=0.5, allowed_missing_percentage=17.0)
-    expected_vector = numpy.array([ 0.12775915, -1.17673617, -0.14598507, -0.40189132, 0.36908966,
+    expected_vector = numpy.array([0.12775915, -1.17673617, -0.14598507, -0.40189132, 0.36908966,
                                    1.11608575, -1.46774333, -0.31442554, -0.23168877, -0.29420064])
     assert document.words[0] not in model.wv.vocab, "Expected word to be missing from given model."
     assert numpy.all(vector == pytest.approx(expected_vector, 1e-5)), "Expected different document vector."
 
 
 def import_pretrained_model():
+    """Helper function to import pretrained word2vec model."""
     repository_root = os.path.join(os.path.dirname(__file__), "..")
     model_file = os.path.join(repository_root, "integration-tests", "test_user_workflow_spec2vec.model")
     return gensim.models.Word2Vec.load(model_file)

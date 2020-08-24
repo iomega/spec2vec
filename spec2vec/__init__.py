b05b17aa27fdc5f1985c17371802b6d456fcd970
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-08-24 18:54:52 +0200

    isort pleasing

--- spec2vec/__init__.py
+++ spec2vec/__init__.py
@@ -1,9 +1,9 @@
 from .__version__ import __version__
-from .vector_operations import calc_vector
 from .Document import Document
 from .Spec2Vec import Spec2Vec
 from .Spec2VecParallel import Spec2VecParallel
 from .SpectrumDocument import SpectrumDocument
+from .vector_operations import calc_vector
 
 
 __all__ = [

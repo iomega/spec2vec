59a7a6152aa1c99fba84058d458e12c84f772d0b
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-09-22 09:36:22 +0200

    remove a few redudant test through parametrize

--- tests/test_vector_operations.py
+++ tests/test_vector_operations.py
@@ -61,114 +61,90 @@ def import_pretrained_model():
     return gensim.models.Word2Vec.load(model_file)
 
 
-def test_cosine_similarity():
+@pytest.mark.parametrize("numba_compiled", [True, False])
+def test_cosine_similarity(numba_compiled):
     """Test cosine similarity score calculation."""
     vector1 = numpy.array([1, 1, 0, 0])
     vector2 = numpy.array([1, 1, 1, 1])
-    score11 = cosine_similarity.py_func(vector1, vector1)
-    score12 = cosine_similarity.py_func(vector1, vector2)
-    score22 = cosine_similarity.py_func(vector2, vector2)
+
+    if numba_compiled:
+        score11 = cosine_similarity(vector1, vector1)
+        score12 = cosine_similarity(vector1, vector2)
+        score22 = cosine_similarity(vector2, vector2)
+    else:
+        score11 = cosine_similarity.py_func(vector1, vector1)
+        score12 = cosine_similarity.py_func(vector1, vector2)
+        score22 = cosine_similarity.py_func(vector2, vector2)
 
     assert score12 == 2 / numpy.sqrt(2 * 4), "Expected different score."
     assert score11 == score22 == 1.0, "Expected different score."
 
 
-def test_cosine_similarity_all_zeros():
+@pytest.mark.parametrize("numba_compiled", [True, False])
+def test_cosine_similarity_all_zeros(numba_compiled):
     """Test cosine similarity score calculation with empty vector."""
     vector1 = numpy.array([0, 0, 0, 0])
     vector2 = numpy.array([1, 1, 1, 1])
-    score11 = cosine_similarity.py_func(vector1, vector1)
-    score12 = cosine_similarity.py_func(vector1, vector2)
-    score22 = cosine_similarity.py_func(vector2, vector2)
+
+    if numba_compiled:
+        score11 = cosine_similarity(vector1, vector1)
+        score12 = cosine_similarity(vector1, vector2)
+        score22 = cosine_similarity(vector2, vector2)
+    else:
+        score11 = cosine_similarity.py_func(vector1, vector1)
+        score12 = cosine_similarity.py_func(vector1, vector2)
+        score22 = cosine_similarity.py_func(vector2, vector2)
 
     assert score11 == score12 == 0.0, "Expected different score."
     assert score22 == 1.0, "Expected different score."
 
 
-def test_cosine_similarity_compiled():
-    """Test cosine similarity score calculation."""
-    vector1 = numpy.array([1, 1, 0, 0])
-    vector2 = numpy.array([1, 1, 1, 1])
-    score11 = cosine_similarity(vector1, vector1)
-    score12 = cosine_similarity(vector1, vector2)
-    score22 = cosine_similarity(vector2, vector2)
-
-    assert score12 == 2 / numpy.sqrt(2 * 4), "Expected different score."
-    assert score11 == score22 == 1.0, "Expected different score."
-
-
-def test_cosine_similarity_all_zeros_compiled():
-    """Test cosine similarity score calculation with empty vector."""
-    vector1 = numpy.array([0, 0, 0, 0])
-    vector2 = numpy.array([1, 1, 1, 1])
-    score11 = cosine_similarity(vector1, vector1)
-    score12 = cosine_similarity(vector1, vector2)
-    score22 = cosine_similarity(vector2, vector2)
-
-    assert score11 == score12 == 0.0, "Expected different score."
-    assert score22 == 1.0, "Expected different score."
-
-
-def test_cosine_similarity_matrix():
+@pytest.mark.parametrize("numba_compiled", [True, False])
+def test_cosine_similarity_matrix(numba_compiled):
     """Test cosine similarity scores calculation using int32 input.."""
     vectors1 = numpy.array([[1, 1, 0, 0],
                             [1, 0, 1, 1]], dtype=numpy.int32)
     vectors2 = numpy.array([[0, 1, 1, 0],
                             [0, 0, 1, 1]], dtype=numpy.int32)
 
-    scores = cosine_similarity_matrix.py_func(vectors1, vectors2)
+    if numba_compiled:
+        scores = cosine_similarity_matrix(vectors1, vectors2)
+    else:
+        scores = cosine_similarity_matrix.py_func(vectors1, vectors2)
     expected_scores = numpy.array([[0.5, 0.],
                                    [0.40824829, 0.81649658]])
     assert scores == pytest.approx(expected_scores, 1e-7), "Expected different scores."
 
 
-def test_cosine_similarity_matrix_compiled():
-    """Test cosine similarity scores calculation using int32 input."""
-    vectors1 = numpy.array([[1, 1, 0, 0],
-                            [1, 0, 1, 1]], dtype=numpy.int32)
-    vectors2 = numpy.array([[0, 1, 1, 0],
-                            [0, 0, 1, 1]], dtype=numpy.int32)
-
-    scores = cosine_similarity_matrix(vectors1, vectors2)
-    expected_scores = numpy.array([[0.5, 0.],
-                                   [0.40824829, 0.81649658]])
-    assert scores == pytest.approx(expected_scores, 1e-7), "Expected different scores."
-
-
-def test_cosine_similarity_floats_matrix():
+@pytest.mark.parametrize("numba_compiled", [True, False])
+def test_cosine_similarity_floats_matrix(numba_compiled):
     """Test cosine similarity scores calculation using float64 input.."""
     vectors1 = numpy.array([[1, 1, 0, 0],
                             [1, 0, 1, 1]], dtype=numpy.float64)
     vectors2 = numpy.array([[0, 1, 1, 0],
                             [0, 0, 1, 1]], dtype=numpy.float64)
 
-    scores = cosine_similarity_matrix.py_func(vectors1, vectors2)
+    if numba_compiled:
+        scores = cosine_similarity_matrix(vectors1, vectors2)
+    else:
+        scores = cosine_similarity_matrix.py_func(vectors1, vectors2)
     expected_scores = numpy.array([[0.5, 0.],
                                    [0.40824829, 0.81649658]])
     assert scores == pytest.approx(expected_scores, 1e-7), "Expected different scores."
 
 
-def test_cosine_similarity_matrix_floats_compiled():
-    """Test cosine similarity scores calculation using float64 input."""
-    vectors1 = numpy.array([[1, 1, 0, 0],
-                            [1, 0, 1, 1]], dtype=numpy.float64)
-    vectors2 = numpy.array([[0, 1, 1, 0],
-                            [0, 0, 1, 1]], dtype=numpy.float64)
-
-    scores = cosine_similarity_matrix(vectors1, vectors2)
-    expected_scores = numpy.array([[0.5, 0.],
-                                   [0.40824829, 0.81649658]])
-    assert scores == pytest.approx(expected_scores, 1e-7), "Expected different scores."
-
-
-def test_cosine_similarity_matrix_input_cloned_compiled():
+@pytest.mark.parametrize("numba_compiled", [True, False])
+def test_cosine_similarity_matrix_input_cloned(numba_compiled):
     """Test if score implementation clones the input correctly."""
     vectors1 = numpy.array([[2, 2, 0, 0],
                             [2, 0, 2, 2]])
     vectors2 = numpy.array([[0, 2, 2, 0],
                             [0, 0, 2, 2]])
 
-    cosine_similarity_matrix(vectors1, vectors2)
+    if numba_compiled:
+        cosine_similarity_matrix(vectors1, vectors2)
+    else:
+        cosine_similarity_matrix.py_func(vectors1, vectors2)
 
     assert numpy.all(vectors1 == numpy.array([[2, 2, 0, 0],
                                               [2, 0, 2, 2]])), "Expected unchanged input."

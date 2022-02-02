70202bc7cf92d88cfef28728a204eb265d1be80a
florian-huber <florian.huber@hs-duesseldorf.de>
2022-02-02 18:19:03 +0100

    isort linting

--- spec2vec/__init__.py
+++ spec2vec/__init__.py
@@ -1,9 +1,9 @@
+from matchms.logging_functions import _init_logger
 from .__version__ import __version__
 from .Document import Document
 from .Spec2Vec import Spec2Vec
 from .SpectrumDocument import SpectrumDocument
 from .vector_operations import calc_vector
-from matchms.logging_functions import _init_logger
 
 
 _init_logger(logger_name="spec2vec")

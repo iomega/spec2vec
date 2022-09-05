01d2748dd1f45697e99890b38475d512544b3ca3
maximskorik <maximskoryk@gmail.com>
2022-09-05 16:51:54 +0200

    add docstrings

--- spec2vec/model_serialization.py
+++ spec2vec/model_serialization.py
@@ -10,7 +10,17 @@ def export_model(model: Word2Vec,
                  output_model_file: Union[str, os.PathLike],
                  output_weights_file: Union[str, os.PathLike]):
     """Write a lightweight version of Spec2Vec model to disk. Such a model can be read to calculate scores
-    but is not fit for further training."""
+    but is not capable of further training.
+
+    Parameters
+    ----------
+    model:
+        Word2Vec trained model.
+    output_model_file:
+        A path to model json file.
+    output_weights_file:
+        A path to model's weights file.
+    """
     keyedvectors_dict = model.wv.__dict__
     keyedvectors_dict = remove_redundant_keys(keyedvectors_dict)
 

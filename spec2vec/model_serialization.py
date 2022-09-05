e3e58cc7ed0fb9fbe154a094813bf34d99e05587
maximskorik <maximskoryk@gmail.com>
2022-09-05 16:47:45 +0200

    refactor `export_model`

--- spec2vec/model_serialization.py
+++ spec2vec/model_serialization.py
@@ -12,19 +12,9 @@ def export_model(model: Word2Vec,
     """Write a lightweight version of Spec2Vec model to disk. Such a model can be read to calculate scores
     but is not fit for further training."""
     keyedvectors_dict = model.wv.__dict__
+    keyedvectors_dict = remove_redundant_keys(keyedvectors_dict)
 
-    # Remove redundant keys
-    vectors = keyedvectors_dict.pop('vectors', ValueError('No vectors found in model')) # is save as npy/npz
-    keyedvectors_dict.pop('vectors_lockf', None) # another numpy array not needed for scoring
-
-    # From gensim/models/keyedvectors.py https://github.com/RaRe-Technologies/gensim/blob/ded78776284ad7b55b6626191eaa8dcea0dd3db0/gensim/models/keyedvectors.py#L248
-    # "expandos" are extra attributes stored for each key: {attribute_name} => numpy array of values of
-    # this attribute, with one array value for each vector key.
-    # The same information used to be stored in a structure called Vocab in Gensim <4.0.0, but
-    # with different indexing: {vector key} => Vocab object containing all attributes for the given vector key.
-    #
-    # Don't modify expandos directly; call set_vecattr()/get_vecattr() instead.
-    keyedvectors_dict.pop('expandos', None)
+    weights = keyedvectors_dict.pop('vectors', ValueError('The model contains no weights.'))
 
     # Save model
     with open(output_model_file, 'w') as f:
@@ -32,8 +22,14 @@ def export_model(model: Word2Vec,
 
     # Save weights
     if keyedvectors_dict['__numpys'] or keyedvectors_dict['__ignoreds']:
-        save_npy(output_weights_file, vectors)
+        save_npy(output_weights_file, weights)
     elif keyedvectors_dict['__scipys']:
-        save_npz(output_weights_file, vectors)
+        save_npz(output_weights_file, weights)
     else:
         raise AttributeError('The model contains no weights.')
+
+
+def remove_redundant_keys(keyedvectors: dict) -> dict:
+    keyedvectors.pop('vectors_lockf', None)
+    keyedvectors.pop('expandos', None)
+    return keyedvectors

52888a841fb5f5d0034f89059de9d73219052c72
maximskorik <maximskoryk@gmail.com>
2022-09-05 17:00:57 +0200

    refactor `export_model` and update docstrings

--- spec2vec/model_serialization.py
+++ spec2vec/model_serialization.py
@@ -9,37 +9,49 @@ from typing import Union
 def export_model(model: Word2Vec,
                  output_model_file: Union[str, os.PathLike],
                  output_weights_file: Union[str, os.PathLike]):
-    """Write a lightweight version of Spec2Vec model to disk. Such a model can be read to calculate scores
+    """Write a lightweight version of a Spec2Vec model to disk. Such a model can be read to calculate scores
     but is not capable of further training.
 
     Parameters
     ----------
     model:
-        Word2Vec trained model.
+        :py:class:Word2Vec trained model.
     output_model_file:
-        A path to model json file.
+        A path of json file to save the model.
     output_weights_file:
-        A path to model's weights file.
+        A path of npy file to save the model's weights.
     """
-    keyedvectors_dict = model.wv.__dict__
-    keyedvectors_dict = remove_redundant_keys(keyedvectors_dict)
-
-    weights = keyedvectors_dict.pop('vectors', ValueError('The model contains no weights.'))
+    keyedvectors = extract_keyedvectors(model)
+    weights = keyedvectors.pop('vectors', ValueError('The model contains no weights.'))
 
     # Save model
     with open(output_model_file, 'w') as f:
-        json.dump(keyedvectors_dict, f)
+        json.dump(keyedvectors, f)
 
     # Save weights
-    if keyedvectors_dict['__numpys'] or keyedvectors_dict['__ignoreds']:
+    if keyedvectors['__numpys'] or keyedvectors['__ignoreds']:
         save_npy(output_weights_file, weights)
-    elif keyedvectors_dict['__scipys']:
+    elif keyedvectors['__scipys']:
         save_npz(output_weights_file, weights)
     else:
         raise AttributeError('The model contains no weights.')
 
 
-def remove_redundant_keys(keyedvectors: dict) -> dict:
+def extract_keyedvectors(model: Word2Vec) -> dict:
+    """
+    Extract :py:class:KeyedVectors object from the model, convert it to a dictionary and remove redundant keys.
+
+    Parameters
+    ----------
+    model:
+        Word2Vec trained model.
+
+    Returns
+    -------
+    keyedvectors:
+        Dictionary representation of :py:class:KeyedVectors without redundant keys.
+    """
+    keyedvectors = model.wv.__dict__
     keyedvectors.pop('vectors_lockf', None)
     keyedvectors.pop('expandos', None)
     return keyedvectors

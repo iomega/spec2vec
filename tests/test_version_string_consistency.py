51a87f6541bd958ecb81e1b723f7ed506c163659
Julian Pollmann <julian.pollmann@hs-duesseldorf.de>
2025-11-10 17:46:39 +0100

    FIx test for version

--- tests/test_version_string_consistency.py
+++ tests/test_version_string_consistency.py
@@ -1,19 +1,23 @@
+import importlib
 import os
+import pytest
 import re
-from spec2vec import __version__ as expected_version
+import tomli
 
 
+@pytest.mark.skipif(not os.getenv("CI"),reason="Skipping version consistency test outside CI environment")
 def test_version_string_consistency():
     """Check whether version in conda/meta.yaml is consistent with that in spec2vec.__version__"""
 
     repository_root = os.path.join(os.path.dirname(__file__), "..")
-    fixture = os.path.join(repository_root, "conda", "meta.yaml")
+    pyproject_file = os.path.join(repository_root, "pyproject.toml")
 
-    with open(fixture, "r", encoding="utf-8") as f:
-        metayaml_contents = f.read()
+    with open(pyproject_file, "rb") as f:
+        pyproject = tomli.load(f)
+    expected_version = pyproject["tool"]["poetry"]["version"]
 
-    match = re.search(r"^{% set version = \"(?P<semver>.*)\" %}$", metayaml_contents, re.MULTILINE)
-    actual_version = match["semver"]
+    spec2vec = importlib.import_module("spec2vec")
+    actual_version = spec2vec.__version__
 
-    assert expected_version == actual_version, "Expected version string used in conda/meta.yaml to be consistent with" \
-                                               " that in spec2vec.__version__"
+    assert expected_version == actual_version, f"Expected version {expected_version!r} in pyproject.toml to match" \
+                                               f" spec2vec.__version__ ({actual_version!r})"
